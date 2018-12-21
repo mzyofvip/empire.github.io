dc98ac62deb580ee7924490f122a513aadce74cd
Empire <mzyofvip@163.com>
2018-12-21 14:53:21 +0800

    add img and edit content

---
+++ download/download.js
@@ -0,0 +1,10 @@
+	document.body.addEventListener('touchstart', function () { });
+
+
+	function myhome() {
+		window.location.href = "https://github.com/mzyofvip";
+	}
+
+	function downloadapp() {
+		window.location.href = "itms-services://?action=download-manifest&url=https://mzyofvip.github.io/download/manifest.plist";
+	}
